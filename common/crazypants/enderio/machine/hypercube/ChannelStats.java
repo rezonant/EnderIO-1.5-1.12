a5603ec69a68e70db9fe9ecd6d0ee217545dd8f6
Liam <liam@aether>
2014-05-03 19:58:33 -0400

    Oops, this is needed for the DT tooltip meters

---
+++ common/crazypants/enderio/machine/hypercube/ChannelStats.java
@@ -0,0 +1,9 @@
+package crazypants.enderio.machine.hypercube;
+
+public class ChannelStats {
+	public int itemsHeld;
+	public int energyHeld;
+	public int inputEnergyMeter;
+	public int outputEnergyMeter;
+	public int transceiverCount;
+}
